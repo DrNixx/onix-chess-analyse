34d47d1c2415d90fb26c076cebad98977f035e8f
DrNixx <drnixx@gmail.com>
2020-12-13 01:39:44 +0300

    Reneme export

--- src/js/index.ts
+++ src/js/index.ts
@@ -2,4 +2,4 @@ export { AnalyseGraph } from './analyse/AnalyseGraph';
 export { AnalyseGraphAsync } from './analyse/AnalyseGraphAsync';
 export { AnalyseGraphProps } from './analyse/AnalyseGraphProps';
 export { AnalysisResult } from './analyse/AnalysisResult';
-export { register } from './i18n';
+export { register as i18nRegister } from './i18n';
